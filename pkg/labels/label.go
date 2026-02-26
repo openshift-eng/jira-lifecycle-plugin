4bf57b0ddd5f7dfe4c8bffc4183722cf450ff89e
Shannon Poole <spoole@redhat.com>
2026-02-26 16:16:37 -0500

    add support to skip dependent bug check

--- pkg/labels/label.go
+++ pkg/labels/label.go
@@ -1,15 +1,16 @@
 package labels
 
 const (
-	JiraValidRef          = "jira/valid-reference"
-	JiraValidBug          = "jira/valid-bug"
-	JiraInvalidBug        = "jira/invalid-bug"
-	QEApproved            = "qe-approved"
-	SeverityCritical      = "jira/severity-critical"
-	SeverityImportant     = "jira/severity-important"
-	SeverityModerate      = "jira/severity-moderate"
-	SeverityLow           = "jira/severity-low"
-	SeverityInformational = "jira/severity-informational"
-	Verified              = "verified"
-	VerifiedLater         = "verified-later"
+	JiraValidRef              = "jira/valid-reference"
+	JiraValidBug              = "jira/valid-bug"
+	JiraInvalidBug            = "jira/invalid-bug"
+	JiraSkipDependentBugCheck = "jira/skip-dependent-bug-check"
+	QEApproved                = "qe-approved"
+	SeverityCritical          = "jira/severity-critical"
+	SeverityImportant         = "jira/severity-important"
+	SeverityModerate          = "jira/severity-moderate"
+	SeverityLow               = "jira/severity-low"
+	SeverityInformational     = "jira/severity-informational"
+	Verified                  = "verified"
+	VerifiedLater             = "verified-later"
 )
